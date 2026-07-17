2239fdd07367f4b71f34ac2e5149cbef264869e0
Bhuvan Kapur <143707945+not-a-slacker@users.noreply.github.com>
2026-07-17 22:22:12 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.cpp
@@ -0,0 +1,89 @@
+class Node {
+    public:
+        int val;
+        vector<Node> neigh;
+
+};
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+
+        for (auto& pre : prerequisites) {
+            indegree[pre[0]]++;
+            adj[pre[1]].push_back(pre[0]);
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int finish = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            finish++;
+            for (int nei : adj[node]) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    q.push(nei);
+                }
+            }
+        }
+
+        return finish == numCourses;
+    }
+};
+// class Solution {
+// public:
+//     vector<vector<int>> prereqs;
+//     unordered_set<int> visited;
+
+//     bool dfs(int i)
+//     {
+//         if (visited.count(i))
+//         {
+//             return false;
+//         }
+//         visited.insert(i);
+//         bool no_cycle=true;
+//         for(int prereq : prereqs[i])
+//         {
+//             no_cycle=dfs(prereq);
+//             if (!no_cycle){
+//                 return false;
+//             }
+//         }
+//         visited.erase(i);
+//         prereqs[i].clear();
+//         return no_cycle;
+//     }
+    
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         for (int i=0;i<numCourses;i++)
+//         {
+//             prereqs.push_back({});
+//         }
+//         for (auto course_prereq : prerequisites)
+//         {
+//             prereqs[course_prereq[0]].push_back(course_prereq[1]);
+//         }
+//         for (int i=0;i<numCourses;i++)
+//         {
+//             if (!dfs(i))
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+
+//     }
+
+        
+
+// };
